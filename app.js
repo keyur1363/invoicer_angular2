bf14be54073a640e4ff5b97e3a982f8457409138
keyur1363 <keyur1363@gmail.com>
2017-08-30 12:53:04 -0700

    angular setup
    angular enviroment setup usign angular cli- and provided with basic
    routing

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const db = mongoose.connection;
 const app = express();
 const port = 3000;
 
+// Enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.static(__dirname + '/client'));
 app.use(bodyParser.json());
 
